c259366f0af652d759ad4cddd52653ee92dcfa2f
LFionina <80356955+LFionina@users.noreply.github.com>
2021-07-02 21:19:32 +0400

    добавлена панель жизни и монет

--- LFioninaTaskThree1.cpp
+++ LFioninaTaskThree1.cpp
@@ -13,7 +13,7 @@
 
 const HDC SPRITE_PERSON  = txLoadImage ("Images\\person.bmp");
 const int X_PERSON = txGetExtentX (SPRITE_PERSON)/4,
-          Y_PERSON = txGetExtentY (SPRITE_PERSON)/3;
+          Y_PERSON = txGetExtentY (SPRITE_PERSON)/4;
 
 //---------------------СТРУКТУРЫ-------------------------------------------------
 
@@ -31,61 +31,11 @@ struct Person
     void Control ();
     };
 
-struct Button
-    {
-    double x, y;
-    char text[15];
-
-    void Draw ();
-    };
-
-struct SubMenu
-    {
-    double x, y;
-    char etemMenu1[14];
-    char etemMenu2[14];
-    char etemMenu3[14];
-
-    void Draw ();
-    };
-
 //-------------------   Ф У Н К Ц И И    К Л А С С О В    ---------------------
 
 
 //- - - - - - - - - - - - рисование героя  - - - - - - - - - - - - - - - - - -
-/*void Person::Draw (int view)
-    {
-    switch (numView)
-        {
-        case 1:
-            {
-            txSetColor     (RGB (84, 0, 0));
-            txSetFillColor (RGB (255, 213, 213));
-            txCircle (x, y, 30);
-            txSetFillColor (RGB (84, 0, 0));
-            txCircle (x, y,  4);
-            }
-            break;
-        case 2:
-            {
-            txSetColor     (RGB (84, 0, 0));
-            txSetFillColor (RGB (255, 213, 213));
-            txRectangle (x - 30, y - 30, x + 30, y + 30);
-            txSetFillColor (RGB (84, 0, 0));
-            txRectangle (x -  4, y -  4, x +  4, y +  4);
-            }
-            break;
-        case 3:
-            {
-            HDC ghost = txLoadImage ("personFront1.bmp");
-            txTransparentBlt  ( txDC(), x + 42, y + 50, 0, 0, ghost, 0, 0, RGB(0,255,0));
-            txDeleteDC (ghost);
-            }
-            break;
-        }
 
-    }
-    */
 void Person::Draw (int view)
     {
     if (course == 1)  //--- вправо
@@ -96,13 +46,17 @@ void Person::Draw (int view)
         {
         txTransparentBlt (txDC(), x, y, X_PERSON, Y_PERSON, SPRITE_PERSON, (view % 4)*X_PERSON, 2*Y_PERSON, RGB(0,255,0));
         }
-    if (course == 0)  //---вверх или вниз
+    if (course == 0)  //---вверх
         {
         txTransparentBlt (txDC(), x, y, X_PERSON, Y_PERSON, SPRITE_PERSON, (view % 4)*X_PERSON, 0*Y_PERSON, RGB(0,255,0));
         }
+    if (course == 3)  //--- вниз
+        {
+        txTransparentBlt (txDC(), x, y, X_PERSON, Y_PERSON, SPRITE_PERSON, (view % 4)*X_PERSON, 3*Y_PERSON, RGB(0,255,0));
+        }
     }
 
-//- - - - - - - - - - - - управление тележкой - - - - - - - - - - - - - - - - -
+//- - - - - - - - - - - - управление героем - - - - - - - - - - - - - - - - -
 void Person::Control ()
     {
     if (txGetAsyncKeyState (VK_RIGHT) )
@@ -118,7 +72,7 @@ void Person::Control ()
     if (txGetAsyncKeyState (VK_UP   ) )
         {
         (*this).y -= 5;
-        (*this).course = 0;
+        (*this).course = 3;
         }
     if (txGetAsyncKeyState (VK_DOWN ) )
         {
@@ -127,59 +81,35 @@ void Person::Control ()
         }
     }
 
-//- - - - - - - - - - - - рисование кнопки  - - - - - - - - - - - - - - - - - -
-void Button::Draw ()
-    {
-    txSetColor (RGB(23, 112, 17));
-    txSetFillColor (RGB(23, 40, 11));
-    txRectangle(x, y, x + 150, y + 40);
-
-    txSetColor (RGB(23, 112, 17));
-    txSetFillColor (RGB(170, 222, 135));
-    txRectangle(x - 3, y - 3, x + 147, y + 37);
-
-    txSetColor(RGB(23, 112, 17));
-    txSelectFont ("Arial Black", 25);
-    txDrawText (x, y+3, x + 150, y + 40, text, DT_CENTER);
-    }
-
-//- - - - - - - - - - - - рисование подменю  - - - - - - - - - - - - - - - - - -
-void SubMenu::Draw ()
-    {
-    txSetColor (RGB(23, 112, 17));
-    txSetFillColor (RGB(23, 112, 17));
-    txRectangle(x, y, x + 170, y + 200);
-
-    txSetColor(RGB(170, 222, 135));
-    txSelectFont ("Arial Black", 25);
-    txDrawText (x + 10, y + 10, x + 160, y +  40, etemMenu1, DT_LEFT);
-    txDrawText (x + 10, y + 50, x + 160, y +  80, etemMenu2, DT_LEFT);
-    txDrawText (x + 10, y + 90, x + 160, y + 120, etemMenu3, DT_LEFT);
-
-    }
-
-
 
 
 //--------------------  ПРОТОТИПЫ ФУНКЦИЙ  ---------------------------------------
 void GameOver (Person hero);
-
+void LiveDraw (Person* hero);
+void MoneyDraw (Person* hero);
 
 //======================== ОСНОВНАЯ ФУНКЦИЯ  ====================================
 int main ()
     {
     txCreateWindow (1300, 800);
 
+    //У персонажа 2 характеристики: жизни и монеты
+    // ____     на старте 100% здоровья и 100 монет
     Person hero = {1090, 231, 1, 1, 100, 100};
+    Person heroOld = hero;
 
-    HDC background = txLoadImage ("Images\\bgfon2.bmp");
+    HDC background = txLoadImage ("Images\\map1.bmp");
     HDC mapBG      = txLoadImage ("Images\\map1.bmp");
 
+    txSetColor (RGB(170, 222, 135), 1);
+    txRectangle(779, 44, 981, 81);
+
     int view  = 0;
-    int d1 = 1;
+    int widthHero  = X_PERSON / 2;
+    int heightHero = Y_PERSON - 10;
 
     // ________ пока не конец игры (не нажата кнопка ESC) _______
-    while (! txGetAsyncKeyState (VK_ESCAPE))
+    while (! txGetAsyncKeyState (VK_ESCAPE) and hero.live > 0 and hero.money > 0)
         {
         txBegin();
         //____ рисуем фон и героя
@@ -188,14 +118,27 @@ int main ()
         view ++;
 
         hero.Draw(view);
+        heroOld = hero;
         hero.Control();
+        LiveDraw  (&hero);
+        MoneyDraw (&hero);
 
-        //____ в противном случае персонаж бродит по полю
-        //     границы отнимают здоровье
+        //____ персонаж бродит по полю - границы отнимают здоровье
+        if (txGetAsyncKeyState (VK_F1)) txBitBlt (0, 0, mapBG);
 
-        //     У персонажа 2 характеристики: жизни и монеты
+        // LT - левая верхняя, RT - правая верхняя, LB - левая нижняя, RB - правая нижняя
+        COLORREF colorControlLT = txGetPixel(hero.x + 5,             hero.y + 5, mapBG);
+        COLORREF colorControlRT = txGetPixel(hero.x + 5 + widthHero, hero.y + 5, mapBG);
+        COLORREF colorControlLB = txGetPixel(hero.x + 5,             hero.y + 5 + heightHero, mapBG);
+        COLORREF colorControlRB = txGetPixel(hero.x + 5 + widthHero, hero.y + 5 + heightHero, mapBG);
 
-        //____     на старте 100% здоровья и 100 монет
+        if ( colorControlLT != RGB (0, 0, 0) and colorControlLB != RGB (0, 0, 0) and
+             colorControlRT != RGB (0, 0, 0) and colorControlRB != RGB (0, 0, 0))
+            {
+            hero = heroOld;
+            hero.live -= 1;
+            }
+
         //____ ___ если натыкается на подарок смотрим какой он по номеру
         //         1 случайно добавляются/удаляются здоровье или деньги
         //         2 требует заплатить за вход, если денег не хватает их
@@ -237,13 +180,40 @@ void GameOver (Person hero)
     txSelectFont ("Arial Black", 40);
 
     char strLive [20] = "";
-    sprintf (strLive,  "монет    : %4d", hero.live);
+    sprintf (strLive,  "монет    : %4d", hero.money);
 
     char strMoney [20] = "";
-    sprintf (strMoney, "здоровье : %4d", hero.money);
+    sprintf (strMoney, "здоровье : %4d", hero.live);
 
     txDrawText (400, 400, 830, 460, strLive,  DT_LEFT);
     txDrawText (400, 480, 830, 540, strMoney, DT_LEFT);
     }
 
+//-----------------------------------------------------------------------------
+void LiveDraw (Person* hero)
+    {
+    int procent = (*hero).live;
+    if (procent > 0)
+        {
+        txSetColor (RGB(170, 222, 135), 1);
 
+        txSetFillColor (RGB(255, 255 - (255 - procent*2.55), 128 - (128 - procent*1.28)));
+        txRectangle(780, 45, 780 + procent*2, 80);
+
+        txSetFillColor (RGB(23, 112, 17));
+        txRectangle(980 - (200 - procent*2), 45, 980, 80);
+        }
+    }
+
+//-----------------------------------------------------------------------------
+void MoneyDraw (Person* hero)
+    {
+    txSetColor (RGB(170, 222, 135));
+
+    txSelectFont ("Arial Black", 40);
+
+    char strMoney [7] = "";
+    sprintf (strMoney, "%4d",(*hero).money);
+
+    txDrawText (500, 45, 600, 80, strMoney,  DT_LEFT);
+    }
